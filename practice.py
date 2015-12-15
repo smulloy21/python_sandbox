72a6908bd5e7a6002957e2eb4b94ea4c5787f5e4
Michael Summers,Shannon Mulloy <student@epicodus.com>
2015-12-15 14:32:05 -0800

    extract relevant data from byte string

--- practice.py
+++ practice.py
@@ -3,13 +3,14 @@ from twisted.internet import reactor, protocol, endpoints
 class UpperProtocol(protocol.Protocol):
 
     def connectionMade(self):
-        self.transport.write(b'Hi! Send me text to convert to uppercase\n')
+        self.transport.write(b'Hi! Send me text to convert to uppercase:\n\n')
 
     def connectionLost(self, reason):
         pass
 
     def dataReceived(self, data):
-        self.transport.write(data.upper())
+        self.transport.write(b'lowercase: ' + data.split()[1] +
+                             b'\nUPPERCASE: ' + data.split()[1].upper())
         self.transport.loseConnection()
 
 factory = protocol.ServerFactory()
