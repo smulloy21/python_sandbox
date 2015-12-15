5301377f2758e91e8f517495f0419123d94577e1
Michael Summers,Shannon Mulloy <student@epicodus.com>
2015-12-15 14:53:17 -0800

    include instructions to interact with server through telnet

--- practice.py
+++ practice.py
@@ -4,13 +4,13 @@ class UpperProtocol(protocol.Protocol):
 
     def connectionMade(self):
         self.transport.write(b'Hi! Send me text to convert to uppercase:\n\n')
+        # ^ inserts the data into a queue
 
     def connectionLost(self, reason):
         pass
 
     def dataReceived(self, data):
-        self.transport.write(b'lowercase: ' + data.split()[1] +
-                             b'\nUPPERCASE: ' + data.split()[1].upper())
+        self.transport.write(data.upper())
         self.transport.loseConnection()
 
 factory = protocol.ServerFactory()
@@ -18,3 +18,15 @@ factory.protocol = UpperProtocol
 
 endpoints.serverFromString(reactor, 'tcp:8000').listen(factory)
 reactor.run()
+
+
+# (with server running in seperate console tab)
+# $ telnet localhost 8000
+# [Trying 127.0.0.1...]
+# [Connected to localhost.]
+# [Escape character is '^]'.]
+# [Hi! Send me text to convert to uppercase:]
+# type something in
+# [TYPE SOMETHING IN]
+# [Connection closed by foreign host.]
+# $
