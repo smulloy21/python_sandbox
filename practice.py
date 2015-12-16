c218083e91ef85d4e6c28dd2c3c10985d9414d95
Michael Summers,Shannon Mulloy <student@epicodus.com>
2015-12-15 17:09:34 -0800

    add ability to count # of connections

--- practice.py
+++ practice.py
@@ -3,21 +3,30 @@ from twisted.internet import reactor, protocol, endpoints
 class UpperProtocol(protocol.Protocol):
 
     def connectionMade(self):
+        self.factory.numProtocols += 1
+        self.transport.write("There are {} connections.\n".format(self.factory.numProtocols).encode())
         self.transport.write(b'Hi! Send me text to convert to uppercase:\n')
         # ^ inserts the data into a queue
 
     def connectionLost(self, reason):
+        self.factory.numProtocols -= 1
         pass
 
     def dataReceived(self, data):
         self.transport.write(data.upper())
-        self.transport.loseConnection()
+        # self.transport.loseConnection()
         # ^ comment out to keep connection running
 
-factory = protocol.ServerFactory()
+def getUpperFactory():
+    factory = protocol.Factory()
+    factory.protocol = UpperProtocol
+    factory.numProtocols = 0
+    return factory
+
+factory = protocol.Factory()
 factory.protocol = UpperProtocol
 
-endpoints.serverFromString(reactor, 'tcp:8000').listen(factory)
+endpoints.serverFromString(reactor, 'tcp:8000').listen(getUpperFactory())
 reactor.run()
 
 
@@ -26,6 +35,7 @@ reactor.run()
 # [Trying 127.0.0.1...]
 # [Connected to localhost.]
 # [Escape character is '^]'.]
+# [There are (numProtocols) connections.]
 # [Hi! Send me text to convert to uppercase:]
 # type something in
 # [TYPE SOMETHING IN]
